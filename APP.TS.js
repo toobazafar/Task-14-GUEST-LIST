38cfbc284bd419ad47817609c074490eb101820d
TOOBA ZAFAR <160070906+toobazafar@users.noreply.github.com>
2024-04-20 00:21:52 +0500

    Add files via upload

---
+++ APP.TS.js
@@ -0,0 +1,4 @@
+var guestList = ['Sidra', 'Laiba', 'Hiba', 'Alaya'];
+for (var i = 0; i < guestList.length; i++) {
+    console.log('I invited to you for dinner tomorrow , Please come my home and make me  happy "THANKS". \n\nHello ' + guestList[i]);
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+let guestList : string [] = ['Sidra','Laiba','Hiba','Alaya'];
+for(let i=0; i<guestList.length;i++){
+    console.log('I invited to you for dinner tomorrow , Please come my home and make me  happy (THANKS). \n\nHello ' + guestList[i]);
+    
+}
